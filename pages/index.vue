942b92caf806223c4c5086d21a7ca66afa634a66
ryota <ryota@Mac.local>
2019-11-12 01:28:39 +0900

    Changed index page by using vuex

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,22 @@
 <template>
   <div>
-    <h1>Welcome ようこそ~ 日本へ~!</h1>
+    <h1>Welcome!</h1>
     <NLink to="/about">
       About page
     </Nlink>
-    <br>
-    <NLink to="/posts/balut">
-      1
-    </Nlink>
-    <br>
-    <NLink to="/posts/whereIsIt">
-      2
-    </NLink>
-    <br>
-    <NLink to="/posts/how">
-      3
-    </NLink>
+    <ul>
+      <li v-for="post in $store.state.posts" v-bind:key="post.id">
+        <a :href="'/posts/'+post.id">
+          {{post.id}}
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
 export default {
   head: {
     title: 'Home page'
-  }
+  },
 }
 </script>
