9f4847ecbc953c442366f2280d256d47abd25b83
ryota <ryota@Mac.local>
2019-11-13 20:53:50 +0900

    the new post function is implemented

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,17 @@
     <h1>Welcome!</h1>
     <NLink to="/about">
       About page
-    </Nlink>
+    </NLink>
     <ul>
-      <li v-for="post in $store.state.posts" v-bind:key="post.id">
-        <a :href="'/posts/'+post.id">
-          {{post.id}}
-        </a>
+      <li v-for="post in posts" v-bind:key="post.id">
+        <NLink :to="`/posts/${post.id}`">
+          {{post.title}}
+        </NLink>
       </li>
     </ul>
+    <NLink to="/new">
+      Add New Post
+    </NLink>
   </div>
 </template>
 <script>
@@ -18,5 +21,13 @@ export default {
   head: {
     title: 'Home page'
   },
+  computed: {
+    posts(){
+      return this.$store.getters.posts
+    },
+  },
+  methods: {
+    }
+  
 }
 </script>
---
+++ pages/new.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h2>Title</h2>
+        <input type="text" v-model="title" >
+        <h2>Content</h2>
+        <textarea v-model="content"></textarea>
+        <button  @click="addNewpage(title,content)" >
+            Create
+        </button>
+    </div>
+</template>
+<script>
+export default {
+    head: {
+        title: 'New Page'
+    },
+    data: function(){
+        return{
+            title: '',
+            content:'',
+        }
+    },
+    methods: {
+       addNewpage(text1,text2){
+           this.$store.dispatch('addNewpage',{id: this.$store.getters.max,title:text1,content:text2})
+       } 
+    }
+
+}
+</script>
+<style scoped>
+input,textarea{
+    color: black;
+    background-color: white;
+}
+</style>
